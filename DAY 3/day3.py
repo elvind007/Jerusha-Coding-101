c132cc1c2808c95913b38b11b377bb16f30985e4
Elvin Dsouza <emdd007@gmail.com>
2022-11-10 23:08:40 +0530

    DAY 4

--- DAY 3/day3.py
+++ DAY 3/day3.py
@@ -2,5 +2,8 @@
 
 #print('   *\n  ***  \n ***** \n*******')
 
-print('t is \"used\" as a tab sequence')
+#print('t is \"used\" as a tab sequence')
 
+name='jerusha'
+
+print('hello, my name is %s' %name)
